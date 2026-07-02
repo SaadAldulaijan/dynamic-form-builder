daed2784f6309ab9b8984e77277cfa2b2a1626d7
SaadAldulaijan <saad.aldulaijan94@gmail.com>
2026-07-02 14:30:43 +0300

    Add Bootstrap layout configuration for dynamic forms

---
+++ src/app/features/dynamic-form/models/layout/form-layout.ts
@@ -0,0 +1,23 @@
+export interface FormLayoutSchema {
+  containerClass?: string;
+}
+
+export interface SectionLayoutSchema {
+  containerClass?: string;
+  titleClass?: string;
+  descriptionClass?: string;
+  fieldsWrapperClass?: string;
+}
+
+export interface FieldLayoutSchema {
+  wrapperClass?: string;
+  labelClass?: string;
+  controlClass?: string;
+  errorClass?: string;
+
+  containerClass?: string;
+  titleClass?: string;
+  fieldsWrapperClass?: string;
+  itemClass?: string;
+  actionsClass?: string;
+}
